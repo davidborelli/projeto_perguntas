0ecfa1b6b9477c5d4d0259261b5dcbcef5d8f990
David Borelli <david_borelli@live.com>
2021-01-27 02:00:42 -0300

    feat: creating Question component

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import './questao.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -27,7 +27,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]),
             ElevatedButton(
               child: Text('Resposta 1'),
               onPressed: _responder,
