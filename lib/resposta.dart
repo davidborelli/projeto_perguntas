392edac90ffacbff8a971cc5feef9a108d82306e
David Borelli <david_borelli@live.com>
2021-01-27 19:52:53 -0300

    feat: passing a function by parameter

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
-
   final String texto;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.quandoSelecionado);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(texto),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+          textStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        child: Text(texto),
+        onPressed: quandoSelecionado,
+      ),
     );
   }
 }
